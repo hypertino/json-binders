90b0e79b0511770c9eb0f45c75972ca14ccb37d3
Maga Abdurakhmanov <maga@inn.eu>
2015-05-31 02:23:10 +0200

    + AnyVal to reduce boxing for implicit classes

--- src/main/scala/eu/inn/binders/json/json.scala
+++ src/main/scala/eu/inn/binders/json/json.scala
@@ -8,19 +8,19 @@ import scala.language.experimental.macros
 import scala.reflect.runtime.universe._
 
 package object json {
-  implicit class JsonStringParser(val jsonString: String) {
+  implicit class JsonStringParser(val jsonString: String) extends AnyVal{
     def parseJson[O]: O = macro JsonMacro.parseJson[O]
   }
 
-  implicit class JsonStringGenerator[O](val obj: O) {
+  implicit class JsonStringGenerator[O](val obj: O) extends AnyVal {
     def toJson: String = macro JsonMacro.toJson[O]
   }
 
-  implicit class JsonStringReader(val inputStream: InputStream) {
+  implicit class JsonStringReader(val inputStream: InputStream) extends AnyVal {
     def readJson[O]: O = macro JsonMacro.readJson[O]
   }
 
-  implicit class JsonStringWriter[O](val obj: O) {
+  implicit class JsonStringWriter[O](val obj: O) extends AnyVal {
     def writeJson(outputStream: OutputStream): Unit = macro JsonMacro.writeJson[O]
   }
 }
